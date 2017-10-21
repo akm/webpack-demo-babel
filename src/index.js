591b2f685e3d7a3e68db274a7794a5b27e15bb70
akm <akm2000@gmail.com>
2017-10-21 11:05:55 +0900

    Add code which use ES2015
    From https://qiita.com/HayneRyo/items/74892d3a37ee96a5df60

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import _ from 'lodash';
+import Person from './person';
 
 function component() {
   var element = document.createElement('div');
@@ -10,3 +11,16 @@ function component() {
 }
 
 document.body.appendChild(component());
+
+class Friend extends Person{
+    constructor(name) {
+      super(name);
+    }
+    callName() {
+      alert(this.name);
+    }
+}
+
+var friend = new Friend('Ryo');
+
+friend.callName();
